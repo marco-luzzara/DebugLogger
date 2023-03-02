19058b9852894d9fca248dbf701dfcc5f183c2b3
trey.hartley <trey.hartley@envisagenow.com>
2023-03-02 13:55:08 -0500

    Patch using method that accepts params to avoid upper-bound on parameter count

--- src/main/DebugLogger/MethodPatcher.cs
+++ src/main/DebugLogger/MethodPatcher.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
-internal class MethodPatcher
+internal static class MethodPatcher
 {
     private static TextWriter GetWriter(object instance)
     {
@@ -22,64 +22,17 @@ internal class MethodPatcher
         return logger?.Configs.OutWriter ?? Console.Out;
     }
 
-    private static void PrintContext(object instance, 
-        MethodBase originalMethod, 
-        params object[] parameters)
+    private static void PrintContext(object __instance, 
+        MethodBase __originalMethod, 
+        params object[] __args)
     {
-        var writer = GetWriter(instance);
-        writer.WriteLine($"{originalMethod.DeclaringType.Name} - {originalMethod.Name}");
+        var writer = GetWriter(__instance);
+        writer.WriteLine($"{__originalMethod.DeclaringType.Name} - {__originalMethod.Name}");
 
-        var paramsInfo = originalMethod.GetParameters();
+        var paramsInfo = __originalMethod.GetParameters();
         for (int i = 0; i < paramsInfo.Length; i++)
         {
-            writer.WriteLine($"{paramsInfo[i].Name}: {parameters[i]}");
+            writer.WriteLine($"{paramsInfo[i].Name}: {__args[i]}");
         }
-    }
-
-    internal static void ContextLog_0(object __instance, MethodBase __originalMethod)
-    {
-        PrintContext(__instance, __originalMethod);
-    }
-
-    internal static void ContextLog_1(object __instance, MethodBase __originalMethod, 
-        object __0)
-    {
-        PrintContext(__instance, __originalMethod, __0);
-    }
-
-    internal static void ContextLog_2(object __instance, MethodBase __originalMethod,
-        object __0, object __1)
-    {
-        PrintContext(__instance, __originalMethod, __0, __1);
-    }
-
-    internal static void ContextLog_3(object __instance, MethodBase __originalMethod, 
-        object __0, object __1, object __2)
-    {
-        PrintContext(__instance, __originalMethod, __0, __1, __2);
-    }
-
-    internal static void ContextLog_4(object __instance, MethodBase __originalMethod,
-        object __0, object __1, object __2, object __3)
-    {
-        PrintContext(__instance, __originalMethod, __0, __1, __2, __3);
-    }
-
-    internal static void ContextLog_5(object __instance, MethodBase __originalMethod, 
-        object __0, object __1, object __2, object __3, object __4)
-    {
-        PrintContext(__instance, __originalMethod, __0, __1, __2, __3, __4);
-    }
-
-    internal static void ContextLog_6(object __instance, MethodBase __originalMethod, 
-        object __0, object __1, object __2, object __3, object __4, object __5)
-    {
-        PrintContext(__instance, __originalMethod, __0, __1, __2, __3, __4, __5);
-    }
-
-    internal static void ContextLog_7(object __instance, MethodBase __originalMethod, 
-        object __0, object __1, object __2, object __3, object __4, object __5, object __6)
-    {
-        PrintContext(__instance, __originalMethod, __0, __1, __2, __3, __4, __5, __6);
     }
 }
